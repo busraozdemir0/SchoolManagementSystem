2f5aa22df4064c7a1e107a89b57280cee332b4b6
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-02-16 17:16:00 +0300

    İletişim kısmındaki adres ve map bilgisi View Componente taşındı

---
+++ BusinessLayer/AutoMapper/Addresses/AddressProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EntityLayer.DTOs.Addresses;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.AutoMapper.Addresses
+{
+    public class AddressProfile:Profile
+    {
+        public AddressProfile()
+        {
+            CreateMap<AddressDto, Address>().ReverseMap();
+        }
+    }
+}
---
+++ EntityLayer/DTOs/Addresses/AddressDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.DTOs.Addresses
+{
+    public class AddressDto
+    {
+        public int Id { get; set; }
+        public string AddressInfo { get; set; }
+        public string EMail { get; set; }
+        public string SupportEMail { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MapLocation { get; set; }
+    }
+}
