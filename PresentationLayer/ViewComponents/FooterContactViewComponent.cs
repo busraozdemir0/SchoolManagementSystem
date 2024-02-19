24651948459bbe8aec9e00c73b7347b2d8a7e2c9
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-02-19 16:13:16 +0300

    Footer düzenlendi

---
+++ PresentationLayer/ViewComponents/FooterContactViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using BusinessLayer.Services.Abstract;
+using DataAccessLayer.UnitOfWorks;
+using EntityLayer.DTOs.Addresses;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.ViewComponents
+{
+    public class FooterContactViewComponent:ViewComponent
+    {
+        private readonly IAddressService _addressService;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FooterContactViewComponent(IAddressService addressService, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _addressService = addressService;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var address = await _unitOfWork.GetRepository<Address>().GetAllAsync();
+
+            var addressFirst = address.First(); // address listesinden ilkini al
+
+            var map = _mapper.Map<AddressDto>(addressFirst); // bu adresi AddressDto'ya map'le
+
+            return View(map);
+        }
+    }
+}
